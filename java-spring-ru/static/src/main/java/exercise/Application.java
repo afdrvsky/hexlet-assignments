64bcc7610892180b8bf3f570f2cbda171f220844
Anton Fedorovsky <anton.fedorovsky@raiffeisen.ru>
2024-11-27 02:22:07 +0300

    submit

---
+++ java-spring-ru/static/src/main/java/exercise/Application.java
@@ -0,0 +1,18 @@
+package exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
